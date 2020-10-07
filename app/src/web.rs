f1f57c57e70e316baab62cf450a747345a9f8dca
xinyuan.yao <yao.ntno@gmail.com>
2020-10-08 03:34:00 +0900

    tmp

--- app/src/web.rs
+++ app/src/web.rs
@@ -2,10 +2,17 @@ use crate::database::{create_pool, DBConn, DBPool, Transaction};
 use crate::domain;
 use crate::domain::*;
 use crate::error::Error;
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::convert::Infallible;
 use std::future::Future;
+
+pub fn to_response<T>(input: Result<T,Error>) -> impl Responder
+where T: Serialize
+{
+    input.map(|x| HttpResponse::Ok().json(x)).map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+}
+
 
 #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
